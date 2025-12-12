f79ebfd90a57da91823501a69901229fd0904257
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-12-12 13:34:02 +0100

    fichier database.sql généré

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'automcompletion';
+$user = 'root';
+$pass = '';
+
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    die("Erreur à la connexion : ". $e->getMessage());
+}
+?>
